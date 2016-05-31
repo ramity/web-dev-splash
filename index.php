cad12adc6d9aa79691dba797cbaf90b631932982
ramity <lcb005@uark.edu>
2016-05-31 11:47:36 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+	<head>
+		<title></title>
+		<link type="text/css" rel="stylesheet" href="http://ramity.com/css/index.css">
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div id="top">
+			Ramity Solutions
+		</div>
+		<div id="nav">
+			<div id="navinr">
+				<div class="navitemactive">HOME</div>
+				<div class="navitem">ABOUT US</div>
+				<div class="navitem">RATES</div>
+				<div class="navitem">PARTS</div>
+				<div class="navitem">CONTACT</div>
+			</div>
+		</div>
+		<div id="home">
+			<div class="centerpos">
+				<div id="homeinr">
+					<img id="homepic" src="https://pbs.twimg.com/profile_images/500839248551895041/rZ4Zjl3A.png">
+					<div id="headername">Lewis Brown</div>
+					<div id="headermoto">Programmer, full stack developer, and yes, pizza expert.</div>
+					<a href="https://www.facebook.com/ramitydotcom">
+						<img class="headersocial" src="http://www.everactive.org/uploads/files/Images/graphics/facebook-white.png">
+					</a>
+					<a href="https://twitter.com/following">
+						<img class="headersocial" src="https://g.twimg.com/Twitter_logo_white.png">
+					</a>
+					<a href="https://plus.google.com/112880562887403561655/posts">
+						<img class="headersocial" src="http://www.limoanywhere.com/images/google.png">
+					</a>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
